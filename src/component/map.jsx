43959965869090ea3ceb1d4099752ef5b134649d
rulo-codes <150222307+rulo-codes@users.noreply.github.com>
2026-04-18 19:47:18 +0800

    Added Wind Layer with @maptiler/weather, with time control and wind data ui

--- src/component/map.jsx
+++ src/component/map.jsx
@@ -1,5 +1,6 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import * as maptilersdk from '@maptiler/sdk';
+import { WindLayer } from '@maptiler/weather';
 import "@maptiler/sdk/dist/maptiler-sdk.css";
 import './map.css';
 
@@ -11,7 +12,20 @@ export default function Map() {
     const zoom = 7;
     maptilersdk.config.apiKey = 'B5DZprXVurPYHQYpjYGc';
 
+    const timeInfo = useRef(null);
+    const timeTextDiv = useRef(null);
+    const timeSlider = useRef(null);
+    const playPauseButton = useRef(null);
+    const pointerDataDiv = useRef(null);
+    const windLayerData = useRef(null);
+    let pointerLngLat = null;
+
+    const [isToggleWeather, setToggleWeather] = useState(false);
+
+
     useEffect(() => {
+
+        //Creating map
         if (map.current) return; // stops map from intializing more than once
         map.current = new maptilersdk.Map({
             container: mapContainer.current,
@@ -20,24 +34,157 @@ export default function Map() {
             zoom: zoom
         });
 
-        function onMove(e){
-            const coords = e.lngLat;
+
+
+        // Add Weather Layer: Wind with Time Slider
+
+        // Create Wind layer
+        const windLayer = new WindLayer();
+
+        map.current.on('style.load', () => {
+            const layers = map.current.getStyle().layers;
+            const firstLabelId = layers.find(l => l.type === 'symbol')?.id;
+
+            map.current.addLayer(windLayer, firstLabelId);
+            map.current.setLayoutProperty(windLayer.id, 'visibility', 'none');
+
+            map.current.once('idle', () => {
+                if (windLayer) {
+                    windLayer.setOpacity(0.7);
+                    console.log("Weather materials initialized successfully.");
+                }
+            });
+        })
+
+        //Time Slider Controls
+        timeSlider.current.addEventListener("input", (evt) => {
+            windLayer.setAnimationTime(parseInt(timeSlider.current.value / 1000))
+        });
+        
+        // Event called when all the datasource for the next days are added and ready.
+        // From now on, the layer nows the start and end dates.
+        windLayer.on("sourceReady", event => {
+            const startDate = windLayer.getAnimationStartDate();
+            const endDate = windLayer.getAnimationEndDate();
+            const currentDate = windLayer.getAnimationTimeDate();
+            refreshTime()
+
+            timeSlider.current.min = +startDate;
+            timeSlider.current.max = +endDate;
+            timeSlider.current.value = +currentDate;
+        });
+
+        // Called when the animation is progressing
+        windLayer.on("tick", event => {
+            refreshTime();
+            updatePointerValue(pointerLngLat);
+        });
+
+        // Called when the time is manually set
+        windLayer.on("animationTimeSet", event => {
+            refreshTime()
+        });
+
+        // When clicking on the play/pause
+        let isPlaying = false;
+        playPauseButton.current.addEventListener("click", () => {
+            if (isPlaying) {
+                    windLayer.animateByFactor(0);
+                    playPauseButton.current.innerText = "Play 3600x";
+            } else {
+                    windLayer.animateByFactor(3600);
+                    playPauseButton.current.innerText = "Pause";
+            }
+
+            isPlaying = !isPlaying;
+        });
+
+        // Update the date time display
+        function refreshTime() {
+            const d = windLayer.getAnimationTimeDate();
+            timeTextDiv.current.innerText = d.toString();
+            timeSlider.current.value = +d;
+        }
+
+        map.current.on('mouseout', function(evt) {
+            if (!evt.originalEvent.relatedTarget) {
+                pointerDataDiv.innerText = "";
+                pointerLngLat = null;
+            }
+        });
+
+        //Update and display wind speed value
+        function updatePointerValue(lngLat) {
+                if (!lngLat) return;
+                pointerLngLat = lngLat;
+                const value = windLayer.pickAt(lngLat.lng, lngLat.lat);
+                
+                if (!value) {
+                    pointerDataDiv.current.innerText = "";
+                    return;
+                }
+                pointerDataDiv.current.innerText = `${value.speedMetersPerSecond.toFixed(1)} m/s`
+        }
+
+        // Function for coordinates on pointer map position
+        function onMove(lngLat){
+            const coords = lngLat;
 
             if(!coordinates.current) return;
             coordinates.current.style.display = "block";
             coordinates.current.innerHTML = `Long: ${coords.lng} <br/> Lat:${coords.lat}`;
         }
 
-        map.current.on('mousemove', onMove);
+        map.current.on('mousemove', (e) => {
+            updatePointerValue(e.lngLat);
+            onMove(e.lngLat);
+        });
 
     }, []);
 
+    function toggleWeatherLayer(){
+        if (!map.current || !map.current.isStyleLoaded()) {
+            console.log("Map style is still loading. Please wait...");
+            return;
+        }
 
+        const layerId = "MapTiler Wind"
+        const currentLayerStatus = map.current.getLayoutProperty(layerId, 'visibility');
+        map.current.setLayoutProperty(layerId, 'visibility', currentLayerStatus === 'none' ? 'visible' : 'none');
+        if(currentLayerStatus === 'none'){
+            console.log("Wind Layer Added");
+            timeInfo.current.style.display = "block";
+            windLayerData.current.style.display = "block";
+        }else{
+            timeInfo.current.style.display = "none";
+            windLayerData.current.style.display = "none";
+        }
+    }
+
+    function toggleWeather(){
+        if(!isToggleWeather){
+            setToggleWeather(true);
+        }else{
+            setToggleWeather(false);
+        }
+
+        toggleWeatherLayer();
+    }
 
     return (
         <div className="map-wrap">
+              <div ref={timeInfo} id="time-info">
+                    <span ref={timeTextDiv} id="time-text"></span>
+                    <button ref={playPauseButton} id="play-pause-bt" className="button">Play 3600x</button>
+                    <input ref={timeSlider} type="range" id="time-slider" min="0" max="11" step="1" />
+                </div>
+                <div ref={windLayerData} id="wind-layer-data">
+                    <div id="variable-name">Wind</div>
+                    <div ref={pointerDataDiv} id="pointer-data"></div>
+                </div>
                 <div ref={mapContainer} className="map" />
                 <pre ref={coordinates} id="coordinates" className="coordinates"></pre>
+                <button id="toggle-weather-btn" onClick={toggleWeather}>Toggle Weather</button>
         </div>
     );
 }
